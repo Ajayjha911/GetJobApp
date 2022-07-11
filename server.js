f6ee87df4770a6ebda6c2ab20a619d40a62582a3
Ajaykumar <ajayindra911@gmail.com>
2022-07-11 16:40:05 +0530

    Changes

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ if (process.env.NODE_ENV !== "production") {
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // only when ready to deploy
-app.use(express.static(path.resolve(__dirname, "/client/build")));
+
+app.use(express.static(path.resolve(__dirname, "app/client/build")));
 
 app.use(express.json());
 app.use(helmet());
@@ -40,7 +41,7 @@ app.use("/api/v1/jobs", authenticateUser, jobRouter);
 
 // only when ready to deploy
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "app/client/build", "index.html"));
 });
 
 app.use(notFoundMiddleware);
